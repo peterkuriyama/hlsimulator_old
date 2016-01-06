1a93b63a1ef7ddce9cb62392052cfddc10ddeea3
peterkuriyama <ptrkrym@uw.edu>
2016-01-05 17:07:41 -0800

    add ability to randomly fill upper left quadrant of matrix

--- R/initialize_population.R
+++ R/initialize_population.R
@@ -12,11 +12,47 @@
 #' put example here dude
 
 initialize_population <- function(nrow, ncol, nfish = 100, distribute, seed = 300){
-  browser()
+  # browser()
   
-  #Initialize area
+  #Create matrix of zeroes
   fishArea <- matrix(0, nrow = nrow, ncol = ncol)
   
+  # Uniformly populate matrix
+  if(distribute == 'uniform'){
+    #Spit out error if nfish is not large enough to have whole fish in each cell
+    # if(nfish %% nrow * ncol != 0) stop('must have whole fish in each cell, adjust nfish, nrow or ncol')
+    
+#     apply(fishArea)
+#     
+#     fishArea <-
+  }
   
   
+  #Populate matrix with fish
+  if(distribute == 'random'){
+    #set seed
+    set.seed(seed)
+    
+    #Populate matrix with samples
+    #For now only does upper left of matrix
+    #Not allowed to populate with more than nfish
+    
+    for(ii in 1:(nrow / 2)){
+      for(jj in 1:(ncol / 2)){
+        samp <- sample(1:10, 1)
+        
+        if(samp > nfish){
+          samp <- nfish
+          nfish <- nfish - samp
+        } else {
+          nfish <- nfish - samp
+        } 
+        
+        fishArea[ii, jj] <- samp
+      }
+    }
+    
+  }
+  
+  return(fishArea)
 }
