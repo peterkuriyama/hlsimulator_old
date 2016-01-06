dabcb9c8e8cc34f6e53da8a0745f11453ea6ca3c
peterkuriyama <ptrkrym@uw.edu>
2016-01-06 14:52:19 -0800

    add fish_population function

--- R/fish_population.R
+++ R/fish_population.R
@@ -1,1 +1,13 @@
-fish_population <- function()
+#'Fish the Population
+#'
+#'Function to fish the population
+#'@param fish.area output from initialize_population function
+#'@param location vector specifying row and column to fish in
+#'@param max.prob probability of catching fish in specified cells
+#'@param delta.prob change in probability getting further from specified location
+
+
+fish_population <- function(fishArea, location, max.prob, delta.prob){
+  
+  
+}
