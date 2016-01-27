c110a15272cdabe699887d84f9247e3f041739cf
peterkuriyama <ptrkrym@uw.edu>
2016-01-27 09:32:21 -0800

    add dummy movement function

---
+++ R/move_fish_none.R
@@ -0,0 +1,16 @@
+#' Move Fish None
+#'
+#' This function doesn't move any fish. Easiest to add no movement into the package
+#' with this dummy function
+#' @param fish_area Input matrix of distributed fish
+#' @keywords movement
+#' @export
+#' @examples 
+#' 
+
+move_fish_none <- function(fish_area){
+  final <- fish_area
+  return(list(init = fish_area, final = final))
+}
+
+
