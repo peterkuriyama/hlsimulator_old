db39c7a5c29f076638740a1b8cb4e6b121df07e1
peterkuriyama <ptrkrym@uw.edu>
2016-07-21 17:32:49 -0700

    start of hotspot movement function

--- R/move_fish_hs.R
+++ R/move_fish_hs.R
@@ -1,15 +1,15 @@
-#' Move Fish to Hotspots
+#' Move Fish Hotspots
 #'
-#' This function is meant to simulate fishing moving offshore. One function call results in 
+#' This function is meant to simulate fish moving towards hotspots. The idea is that rockfish
+#' tend to aggregate around rocky structure
+#' One function call results in 
 #' fish moving one column to the left with some probability (specified in the arguments). 
-#' Probabilities decline evenly by column (highest on furthest left column). Note that the movement
-#' is determined by the number of fish surrounding the designated hotspots. 
-
+#' Probabilities decline evenly by column (highest on furthest left column).
 #' @param fish_area Input matrix of distributed fish
-#' @param max_prob Maximum probability of moving towards hotspot, highest in cells adjacent to hotspot
-#' @param hs_locs Locations of hotspots that fish are attracted to
-#' @param hs_prob Probability of fish moving away from hotspot, default to 0.1
+#'Need to fill all these in
 
+#' @param max_prob maximum movement probability in the furthest left location
+#' @param min_prob minimum movement probability in furthest right location
 #' @keywords movement
 #' @export
 #' @examples 
@@ -18,144 +18,69 @@
 fish_area <- initialize_population(distribute = 'patchy', numrow = 10, numcol = 10, seed = 5,
   nfish = 1000, percent = .50)
 
-hs_locs <- data.frame("row" = c(4, 2), "column" = c(7, 10))
-max_prob <- .3
-out_prob <- .1
-seed <- 300
 
-move_fish_hs <- function(fish_area, max_prob, hs_locs , seed = 300, out_prob = .1, 
-  ...){
 
+
+move_fish_hs <- function(fish_area, max_prob = 0.5, min_prob = 0.05, ...){
   numrow <- nrow(fish_area)
   numcol <- ncol(fish_area)
-  
+
   #Convert fish_area into a data frame to facilitate calculations
   fish_area_df <- melt(fish_area)
   names(fish_area_df) <- c('row', 'column', 'nfish')
 
-  #create indices of pasted row and column
-  fish_area_df$ind <- paste(fish_area_df$row, fish_area_df$column)
-  hs_locs$ind <- paste(hs_locs$row, hs_locs$column)
-
-  #Define probability of moving
-  hs_inds <- which(fish_area_df$ind %in% hs_locs$ind) #indices of hotspots
   
-  #First cut subset cells that are 1 away
-  first <- subset(fish_area_df, row )
+  nfish_outside
 
 
-  ##If there are multiple hotspots, move fish in a for loop for each hotspot
-  for(ii in 1:nrow(hs_locs)){
-    
-    #Define probabilities of moving based on proximity to the hotspot
-    x <- hs_locs[ii, ]
-    firsts <- expand.grid((as.numeric(x[1]) - 1):(as.numeric(x[1]) + 1),
-      (as.numeric(x[2]) - 1):(as.numeric(x[2]) + 1))
-    firsts$ind <- paste(firsts$Var1, firsts$Var2)
-      
-    fish_area_df[fish_area_df$ind %in% firsts$ind, 'prob'] <- max_prob
 
-    seconds <- expand.grid((as.numeric(x[1]) - 2):(as.numeric(x[1]) + 2),
-          (as.numeric(x[2]) - 2):(as.numeric(x[2]) + 2))
-    seconds$ind <- paste(seconds$Var1, seconds$Var2)
-    seconds <- seconds[-which(seconds$ind %in% firsts$ind), ]
-    
-    #Here I define the probability of moving for cells 2 away. Might need to change this
-    fish_area_df[fish_area_df$ind %in% seconds$ind, 'prob'] <- max_prob / 2
-    fish_area_df[fish_area_df$ind == x$ind, 'prob'] <- out_prob
-
-    set.seed(seed)
-
-    #Sample # moving fish from the number of fish in each cell
-    fish_area_df$sample <- rbinom(n = length(fish_area_df$nfish),
-      size = fish_area_df$nfish, prob = fish_area_df$prob)
-
-    #Update the cells based on the draws, except for the 
-    fish_area_df$nfish_new <- fish_area_df$nfish - fish_area_df$sample
-
-    #Change the hotspot number of fish back to original
-    hotspot <- which(fish_area_df$ind %in% x$ind )
-    fish_area_df[hotspot, 'nfish_new'] <- fish_area_df[hotspot, 'nfish']
-    
-    #Randomly pick one of the firsts places to move the sampled fish from the hotspot
-    picked_spot <- firsts[base::sample(1:nrow(firsts), 1), 'ind']
-    
-
-    fish_area_df[fish_area_df$ind == picked_spot, 'nfish_new'] <- 
-      fish_area_df[fish_area_df$ind == picked_spot, 'nfish_new'] + fish_area_df[hotspot, 'sample']
-    fish_area_df[hotspot, 'nfish_new'] <- fish_area_df[hotspot, 'nfish_new'] - fish_area_df[hotspot, 'sample']
-
-    new_fish_area <- matrix(fish_area_df$nfish_new, nrow = nrow(fish_area), ncol = ncol(fish_area))
+  return
 
 
-  }
 
 
+
+
+  #Define movement probabilities based on specified max and min probs
+  col_fish <- colSums(fish_area) #summed fish by columns
+  prob_fish <- col_fish
+  probs <- seq(max_prob, min_prob, length.out = length(which(col_fish != 0)))
+  prob_fish[prob_fish != 0] <- probs
   
+  #Sample number of moving fish based on column
+  fish_area_df$sampled <- apply(fish_area_df, MAR = 1, function(x) rbinom(n = 1, size = x['nfish'], 
+    prob = prob_fish[x[2]]))
+  fish_area_df$updated <- fish_area_df$nfish - fish_area_df$sampled
+
+  #Split fish_area_df into two data columns
+  #subtract 1 from columns in moving df
+  #merge the two and update nfish  
+  orig <- fish_area_df[, c('row', 'column', 'updated')]
+  moving <- fish_area_df[, c('row', 'column', 'sampled')]
+  moving$column <- moving$column - 1
+  moving[which(moving$column == 0), 'column'] <- 1
+  moving$column <- as.integer(moving$column)
   
+  moving %>% group_by(row, column) %>% mutate(sampled = sum(sampled))
+
+  moving %>% 
+              group_by(row, column) %>% 
+              summarise(sampled = sum(sampled)) %>% 
+              as.data.frame
+
+
+  moving <- moving %>% 
+              group_by(row, column) %>% 
+              summarise(sampled = sum(sampled)) %>% 
+              as.data.frame
+
+  out <- merge(orig, moving, by = c('row', 'column'), all = TRUE)
+  out[is.na(out$sampled), 'sampled'] <- 0
+  out$moved <- out$updated + out$sampled
+
+  final <- matrix(out$moved, nrow = numrow, ncol = numcol, byrow = T)
   
-
-
-
-firsts <- expand.grid((as.numeric(x[1]) - 1):(as.numeric(x[1]) + 1),
-      (as.numeric(x[2]) - 1):(as.numeric(x[2]) + 1))
-firsts$ind <- paste(firsts$Var1, firsts$Var2)
-  
-fish_area_df[fish_area_df$ind %in% firsts$ind, 'prob'] <- max_prob
-
-seconds <- expand.grid((as.numeric(x[1]) - 2):(as.numeric(x[1]) + 2),
-      (as.numeric(x[2]) - 2):(as.numeric(x[2]) + 2))
-seconds$ind <- paste(seconds$Var1, seconds$Var2)
-seconds <- seconds[-which(seconds$ind %in% firsts$ind), ]
-fish_area_df[fish_area_df$ind %in% seconds$ind, 'prob'] <- max_prob / 2
-
-matrix(fish_area_df$prob, nrow = 10, ncol = 10) 
-  
-  #sample fish in each cell then move to corresponding place in cw region
-  set.seed(seed)
-  fish_area_df$sample <- rbinom(n = length(fish_area_df$nfish),
-    size = fish_area_df$nfish, prob = move_prob)
-  fish_area_df$nfish_left <- fish_area_df$nfish - fish_area_df$sample
-
-  moving_fish <- fish_area_df
-  moving_fish$nfish <- NULL
-  moving_fish$nfish_left <- NULL
-  
-  #move fish based on what quadrant they're in  
-  #upper left move to lower left
-  moving_fish[which(moving_fish$quadrant == 'ul'), 'row'] <- 
-    moving_fish[which(moving_fish$quadrant == 'ul'), 'row'] + numrow / 2
-
-  #lower left move to lower right
-  moving_fish[which(moving_fish$quadrant == 'll'), 'column'] <- 
-    moving_fish[which(moving_fish$quadrant == 'll'), 'column'] + numcol / 2
-
-  #lower right move to upper right
-  moving_fish[which(moving_fish$quadrant == 'lr'), 'row'] <- 
-    moving_fish[which(moving_fish$quadrant == 'lr'), 'row'] - numrow / 2
-
-  #upper right move to upper left
-  moving_fish[which(moving_fish$quadrant == 'ur'), 'column'] <- 
-    moving_fish[which(moving_fish$quadrant == 'ur'), 'column'] - numcol / 2
-
-
-  merged_fish <- merge(fish_area_df[, c('row', 'column', 'nfish_left')],
-                   moving_fish[, c('row', 'column', 'sample')], 
-                   by = c('row', 'column'), all = TRUE)
-
-
-  merged_fish$final_fish <- merged_fish$nfish_left + merged_fish$sample
-
-
-  out <- matrix(0, nrow = numrow, ncol = numcol)
-  for(rr in 1:nrow(merged_fish)){
-    temp <- merged_fish[rr, ]
-    out[temp$row, temp$col] <- temp$final_fish
-  }
-
-  return(list(init = fish_area, final = out))
+  return(list(init = fish_area, final = final))
 }
 
-  
-  
 
